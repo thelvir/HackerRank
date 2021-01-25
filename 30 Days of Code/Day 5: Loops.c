b18cc99fae528f1a139b5c3c3d50af06e81b2db0
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-25 17:19:05 +0400

    Create Day 5: Loops.c

---
+++ 30 Days of Code/Day 5: Loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    int sum;
+    scanf("%d",&a);
+    for(int i=1;i<=10;i++)
+    {
+    printf("%d x %d = %d\n",a,i,a*i);
+    
+    }
+      
+    return 0;
+}
