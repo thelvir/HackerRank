87459f3ac248f0b88763680056cd924100c6c6c6
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-25 17:21:46 +0400

    Create Day 10: Binary Numbers.java

---
+++ 30 Days of Code/Day 10: Binary Numbers.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int rem=0,s=0,t=0;
+
+
+    while(n>0)
+        {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+
+    System.out.println(t);
+}
+}
