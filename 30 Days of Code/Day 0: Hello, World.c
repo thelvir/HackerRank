8c6f6fd29f6c1403594d4203473fbe85bca8385f
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-25 17:14:05 +0400

    Create Day 0: Hello, World.c

---
+++ 30 Days of Code/Day 0: Hello, World.c
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    char bilal[100000];
+    gets(bilal);
+    printf("Hello, World.\n");
+    printf("%s",bilal);
+    return 0;
+}
