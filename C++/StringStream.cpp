fb79ce6b65c0b4d6436c432e0d55b502c52cd0f5
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-25 16:00:20 +0400

    Create StringStream.cpp

---
+++ C++/StringStream.cpp
@@ -0,0 +1,16 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+   string a;
+cin>>a;
+for (int i=0;i<a.size();i++)
+{
+    if (a[i]!=',')
+        cout<<a[i];
+    else
+        cout<<"\n";    
+}
+}
