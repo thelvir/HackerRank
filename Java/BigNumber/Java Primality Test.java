5dc73ee7d4fb48b8986d2ea18a0c486fc31c33b2
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-26 13:16:50 +0400

    Create Java Primality Test.java

---
+++ Java/BigNumber/Java Primality Test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}
