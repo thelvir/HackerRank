64828e384132c5e3895ceb91b91aebaca1ffc74d
thelvir <77773439+thelvir@users.noreply.github.com>
2021-01-26 09:50:48 +0400

    Create Java Date and Time.java

---
+++ Java/Java Date and Time.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+class Result {
+
+public static String findDay(int month,int day,int year){
+    Calendar calendar=Calendar.getInstance();
+    calendar.set(Calendar.MONTH,month-1);
+    calendar.set(Calendar.DAY_OF_MONTH,day);
+    calendar.set(Calendar.YEAR,year);
+    return  calendar.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG,Locale.US).toUpperCase();
+}
+
+}
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
